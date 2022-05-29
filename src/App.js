6a0ad4fd398bd7d62188d1784403cf46feec4b3e
viktorwebjs <viktor.webjs@gmail.com>
2022-05-29 11:09:52 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
-import Main from './pages/Main';
+import { Routes, Route } from 'react-router-dom';
+import Main from './pages/main/Main';
+import Blog from './pages/blog/Blog';
+import AboutUs from './pages/about/AboutUs';
+import Contact from './pages/contact/Contact';
+import Error from './pages/error/Error';
+import Header from './components/header/Header';
+
 function App() {
   return (
-    <div className="App">
-      <Main />
-    </div>
+    <>
+      <div className="App">
+        <div className="header">
+          <Header />
+        </div>
+        <Routes>
+          <Route index path="" element={<Main />} />
+          <Route path="AboutUs" element={<AboutUs />} />
+          <Route path="Blog" element={<Blog />}></Route>
+          <Route path="Contact" element={<Contact />}></Route>
+          <Route path="*" element={<Error />}></Route>
+        </Routes>
+      </div>
+    </>
   );
 }
 
